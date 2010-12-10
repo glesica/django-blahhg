3aeb49da19a3ff4d11547fdac1c24741e7645e94
George Lesica <glesica@gmail.com>
2010-12-10 00:19:35 -0600

    Reorganized urls for list and detail. Using generic views now.

--- blahhg/urls.py
+++ blahhg/urls.py
@@ -1,6 +1,19 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-urlpatterns = patterns('django.views.generic.simple',
-    (r'^$',               'direct_to_template', {'template': 'blahhg/entry_list.html'}),
-    (r'^(?P<slug>[a-z0-9-]+)/$', 'direct_to_template', {'template': 'blahhg/entry_detail.html'}),
+from blahhg.models import BlahhgEntry
+
+urlpatterns = patterns('django.views.generic',
+    (r'^$', 'list_detail.object_list', {
+        'queryset':             BlahhgEntry.live.all(),
+        'template_name':        'blahhg/blog.html',
+        'paginate_by':          settings.BLAHHG_LIST_SIZE,
+        'page':                 1,
+        'template_object_name': 'entry',
+    }),
+    (r'^archive/(?P<slug>[a-z0-9-]+)/$', 'list_detail.object_detail', {
+        'queryset':             BlahhgEntry.live.all(),
+        'template_name':        'blahhg/entry.html',
+        'template_object_name': 'entry',
+    }),
 )

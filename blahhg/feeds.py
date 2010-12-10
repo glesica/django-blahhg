e131e59f556c9157e50b38d382950d003d183c16
George Lesica <glesica@gmail.com>
2010-12-10 16:19:13 -0600

    Added feeds.

---
+++ blahhg/feeds.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from blahhg.models import BlahhgEntry
+
+class RssFeed(Feed):
+    title = settings.BLAHHG_TITLE
+    link = reverse('blahhg-blog')
+    description = settings.BLAHHG_DESCRIPTION
+    
+    def items(self):
+        return BlahhgEntry.live.all()[:settings.BLAHHG_FEED_SIZE]
+
+class AtomFeed(RssFeed):
+    feed_type = Atom1Feed
+    subtitle = RssFeed.description

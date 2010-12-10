8b68c43ce389973cd87e655063a1b58f7c0fa0f9
George Lesica <glesica@gmail.com>
2010-12-10 16:18:50 -0600

    Added named urls. Not working though for some reason.

--- blahhg/urls.py
+++ blahhg/urls.py
@@ -2,17 +2,20 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 from blahhg.models import BlahhgEntry
+from blahhg.feeds import RssFeed, AtomFeed
 
 urlpatterns = patterns('django.views.generic',
-    (r'^$', 'list_detail.object_list', {
+    url(r'^$', 'list_detail.object_list', {
         'queryset':             BlahhgEntry.live.all(),
         'template_name':        'blahhg/blog.html',
         'paginate_by':          settings.BLAHHG_LIST_SIZE,
         'template_object_name': 'entry',
-    }),
-    (r'^archive/(?P<slug>[a-z0-9-]+)/$', 'list_detail.object_detail', {
+    }, name='blahhg-blog'),
+    url(r'^feeds/rss/$', RssFeed()),
+    url(r'^feeds/atom/$', AtomFeed()),
+    url(r'^archive/(?P<slug>[a-z0-9-]+)/$', 'list_detail.object_detail', {
         'queryset':             BlahhgEntry.live.all(),
         'template_name':        'blahhg/entry.html',
         'template_object_name': 'entry',
-    }),
+    }, name='blahhg-detail'),
 )
